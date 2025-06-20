d0198267a09a0714028b203a130817a91a514ff7
Tray <tracymedina.dev@gmail.com>
2025-06-20 22:10:14 +0100

    added algorithm 4 and 5

---
+++ src/algoritmos/4.js
@@ -0,0 +1,10 @@
+//Crear un algoritmo que devuelva el factorial de un número.
+
+let i = 5;
+let multi = 1;
+while (i >= 1) {
+  multi = multi * i;
+  --i;
+}
+
+console.log(multi);
---
+++ src/algoritmos/5.js
@@ -0,0 +1,30 @@
+//5 - Determinar si una palabra es un palíndromo.
+
+const word = "reconocer";
+let reverseWord = "";
+
+reverseWord = word.split("");
+reverseWord = reverseWord.reverse();
+reverseWord = reverseWord.join("");
+
+console.log(word === reverseWord);
+
+//versión función
+// function isPalindromo() {
+//   let word = prompt("Write a word").toLowerCase();
+//   const regex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ]+$/;
+
+//   if (word && word.length >= 2 && regex.test(word)) {
+//     let reverseWord = word.split("").reverse().join("");
+
+//     if (word === reverseWord) {
+//       alert(`Your word is a Palindromo`);
+//     } else {
+//       alert(`Your word is not a Palindromo`);
+//     }
+//   } else {
+//     alert("Make an effort. Write a real word.");
+//   }
+// }
+
+// isPalindromo();
