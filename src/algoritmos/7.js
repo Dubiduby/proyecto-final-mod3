2579a2d845a19e2004a94d9e84d5cfac8572d9d5
Tray <tracymedina.dev@gmail.com>
2025-06-21 10:22:16 +0100

    added algorithm 7

--- src/algoritmos/7.js
+++ src/algoritmos/7.js
@@ -1,1 +1,12 @@
 //Ordenar un array de números en orden ascendente (sin usar sort).
+
+let numbers = [5, 7, 2, 4, 9, 0];
+
+for (let i = 0; i < numbers.length; i++) {
+  for (let x = i + 1; x < numbers.length; x++) {
+    if (numbers[i] > numbers[x]) {
+      [numbers[i], numbers[x]] = [numbers[x], numbers[i]];
+    }
+  }
+}
+console.log(numbers);
